83c407787d7eb76bd1f3709ee5ad7cadacf06830
Douglas Craig <dcraig112@gmail.com>
2018-05-19 13:09:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,5 +4,7 @@
 def position_taken?(board, index)
   if board[index] = "x" || board[index] = "X" || board[index] = "o" || board[index] = "O"
     true
+  elsif board[index] = "" || board[index] = " " || board[index] = nil
+    false
   end
 end
